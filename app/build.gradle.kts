048545ffb7c1346bca4a6be8e8f0b1d3f707c70d
julia <j.kholstinina@yandex.ru>
2022-07-05 22:11:14 +0300

    move to Kotlin DSL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,54 +52,58 @@ android {
 
     dependencies {
 
-        implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.4.32")
-        implementation ("androidx.core:core-ktx:1.7.0")
-        implementation ("androidx.appcompat:appcompat:1.3.1")
-        implementation ("com.google.android.material:material:1.4.0")
-        implementation ("androidx.constraintlayout:constraintlayout:2.1.1")
-        implementation ("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
+        //Kotlin
+        implementation (Kotlin.stdLib)
+        implementation (Kotlin.core)
+
+        // Design
+        implementation (Design.androidxAppcompat)
+        implementation (Design.material)
+        implementation (Design.androidxConstraintlayout)
+        implementation (Design.coordinatorLayout)
+
+        //Test
+        testImplementation (Testing.junit)
+        androidTestImplementation (Testing.testJunit)
+        androidTestImplementation (Testing.espressoTest)
 
         //Moxy
-        implementation ("com.github.moxy-community:moxy:2.2.2")
-        kapt ("com.github.moxy-community:moxy-compiler:2.2.0")
-        implementation ("com.github.moxy-community:moxy-androidx:2.2.2")
-        implementation ("com.github.moxy-community:moxy-ktx:2.2.2")
+        implementation (Moxy.moxy)
+        kapt (Moxy.moxyKaptCompiler)
+        implementation (Moxy.moxyAndoidX)
+        implementation (Moxy.moxyKtx)
 
         //recycler
-        implementation ("androidx.recyclerview:recyclerview:1.2.1")
+        implementation (Recycler.rv)
 
         //Cicerone
-        implementation("com.github.terrakok:cicerone:7.1")
+        implementation(Cicerone.cicerone)
 
         //RX
-        implementation ("io.reactivex.rxjava3:rxandroid:3.0.0")
-        implementation ("io.reactivex.rxjava3:rxjava:3.1.2")
+        implementation (RXJava3.rxJava3Android)
+        implementation (RXJava3.rxJava3)
 
         //Retrofit
-        implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-        implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-        implementation ("com.squareup.retrofit2:adapter-rxjava3:2.9.0")
+        implementation (Retrofit.retrofit)
+        implementation (Retrofit.converterGson)
+        implementation (Retrofit.rxJava3Adapter)
 
         //Glide
-        implementation ("com.github.bumptech.glide:glide:4.12.0")
-        kapt ("com.github.bumptech.glide:compiler:4.12.0")
+        implementation (Glide.glide)
+        kapt (Glide.glideKapt)
 
         //Room
-        implementation ("androidx.room:room-runtime:2.3.0")
-        kapt ("androidx.room:room-compiler:2.3.0")
+        implementation (Room.runtime)
+        kapt (Room.compilerKapt)
 
         //Stetho
-        implementation ("com.facebook.stetho:stetho:1.6.0")
-        implementation ("com.facebook.stetho:stetho-urlconnection:1.6.0")
-        implementation ("com.facebook.stetho:stetho-js-rhino:1.6.0")
-        implementation ("com.facebook.stetho:stetho-okhttp3:1.6.0")
+        implementation (Stetho.stetho)
+        implementation (Stetho.stethoUrlConnection)
+        implementation (Stetho.stethoJsRhino)
+        implementation (Stetho.stethoOkhttp3)
 
         //Dagger
-        implementation ("com.google.dagger:dagger:2.40")
-        kapt ("com.google.dagger:dagger-compiler:2.24")
-
-        testImplementation ("junit:junit:4.+")
-        androidTestImplementation ("androidx.test.ext:junit:1.1.3")
-        androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
+        implementation (Dagger.dagger)
+        kapt (Dagger.daggerKapt)
     }
 }
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -13,27 +13,28 @@ object Releases {
 
 object Versions {
     const val ktxVersion = "1.7.0"
-    const val kotlinStdLibVersion = "1.4.2"
-    const val rxJava2AndroidVersion = "2.1.0"
-    const val rxJava2Version = "2.1.0"
-    const val retrofit2Version = "2.6.0"
-    const val converterGsonVersion = "2.6.0"
-    const val okhhtp3LoggingInterceptorVersion = "3.12.1"
-    const val retrofitAdapterVersion = "1.0.0"
-    const val coroutinesVersion = "1.5.1"
-    const val coroutinesAndroidVersion = "1.5.0"
-    const val coroutinesAdapter = "0.9.2"
+    const val kotlinStdLibVersion = "1.4.32"
+    const val rxJava3AndroidVersion = "3.0.0"
+    const val rxJava3Version = "3.1.2"
+    const val retrofit2Version = "2.9.0"
+    const val converterGsonVersion = "2.9.0"
+    const val retrofitAdapterVersion = "2.9.0"
     const val appcompatVersion = "1.3.1"
     const val materialVerson = "1.4.0"
     const val constraintlayoutVersion = "2.1.1"
     const val junitVersion = "1.1.3"
+    const val junitTestVersion = "4.13.2"
     const val epressoCoreVersion = "3.4.0"
-    const val koinVersion = "3.1.2"
-    const val picassoVersion = "2.71828"
+    const val glideVersion = "4.12.0"
     const val roomVersion = "2.3.0"
     const val stethoVersion = "1.6.0"
-    const val exoPlayer = "2.16.1"
-
+    const val dagger = "2.40"
+    const val daggerKapt = "2.24"
+    const val moxyVersion = "2.2.2"
+    const val moxyAndroidXVersion = "2.2.2"
+    const val ciceroneVersion = "7.1"
+    const val recyclerVersion = "1.2.1"
+    const val coordinatorLayoutVersion = "1.2.0"
 }
 
 object Kotlin {
@@ -46,9 +47,68 @@ object Design {
     const val material = "com.google.android.material:material:${Versions.materialVerson}"
     const val androidxConstraintlayout =
         "androidx.constraintlayout:constraintlayout:${Versions.constraintlayoutVersion}"
+    const val coordinatorLayout =
+        "androidx.coordinatorlayout:coordinatorlayout:${Versions.coordinatorLayoutVersion}"
 }
 
 object Testing {
     const val testJunit = "androidx.test.ext:junit:${Versions.junitVersion}"
     const val espressoTest = "androidx.test.espresso:espresso-core:${Versions.epressoCoreVersion}"
+    const val junit = "junit:junit:${Versions.junitTestVersion}"
 }
+
+object RXJava3 {
+    const val rxJava3Android = "io.reactivex.rxjava3:rxandroid:${Versions.rxJava3AndroidVersion}"
+    const val rxJava3 = "io.reactivex.rxjava3:rxjava:${Versions.rxJava3Version}"
+}
+
+object Stetho {
+    const val stetho = "com.facebook.stetho:stetho:${Versions.stethoVersion}"
+    const val stethoUrlConnection =
+        "com.facebook.stetho:stetho-urlconnection:${Versions.stethoVersion}"
+    const val stethoJsRhino = "com.facebook.stetho:stetho-js-rhino:${Versions.stethoVersion}"
+    const val stethoOkhttp3 = "com.facebook.stetho:stetho-okhttp3:${Versions.stethoVersion}"
+}
+
+object Dagger {
+    const val dagger = "com.google.dagger:dagger:${Versions.dagger}"
+    const val daggerKapt = "com.google.dagger:dagger-compiler:${Versions.daggerKapt}"
+}
+
+object Retrofit {
+    const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit2Version}"
+    const val converterGson =
+        "com.squareup.retrofit2:converter-gson:${Versions.converterGsonVersion}"
+    const val rxJava3Adapter =
+        "com.squareup.retrofit2:adapter-rxjava3:${Versions.retrofitAdapterVersion}"
+}
+
+object Room {
+    const val runtime = "androidx.room:room-runtime:${Versions.roomVersion}"
+    const val compilerKapt = "androidx.room:room-compiler:${Versions.roomVersion}"
+}
+
+object Glide {
+    const val glide = "com.github.bumptech.glide:glide:${Versions.glideVersion}"
+    const val glideKapt = "com.github.bumptech.glide:compiler:${Versions.glideVersion}"
+}
+
+object Moxy {
+    const val moxy = "com.github.moxy-community:moxy:${Versions.moxyVersion}"
+    const val moxyKaptCompiler = "com.github.moxy-community:moxy-compiler:${Versions.moxyVersion}"
+    const val moxyAndoidX =
+        "com.github.moxy-community:moxy-androidx:${Versions.moxyAndroidXVersion}"
+    const val moxyKtx = "com.github.moxy-community:moxy-ktx:${Versions.moxyAndroidXVersion}"
+}
+
+object Cicerone {
+    const val cicerone = "com.github.terrakok:cicerone:${Versions.ciceroneVersion}"
+}
+
+object Recycler {
+    const val rv = "androidx.recyclerview:recyclerview:${Versions.recyclerVersion}"
+}
+
+
+
+
